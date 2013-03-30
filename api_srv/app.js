53b3a69b6b980736dc772785c0a64f3d130bf080
Alexandr Pokutnik <pokutnik@gmail.com>
2013-03-30 17:54:12 +0200

    add example index page

--- api_srv/app.js
+++ api_srv/app.js
@@ -15,11 +15,10 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieParser('secret'));
   app.use(express.session());
   app.use(app.router);
   app.use(require('less-middleware')({ src: __dirname + '/public' }));
